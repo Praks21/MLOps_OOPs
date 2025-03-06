c6bebd409842601dbac7d54e1da0910bc8c2eaf7
Praks21 <101043744+Praks21@users.noreply.github.com>
2025-03-06 20:50:01 +0530

    project basic structure ready

---
+++ oops_proj.py
@@ -0,0 +1,27 @@
+class chitchat:
+    def __init__(self):
+        self.username=""
+        self.password=""
+        self.loggedin=False
+        self.menu()
+    
+    def menu(self):
+        user_input=input("""welcome to chitchat!! how would you like to proceed?
+                         1. Press 1 to signup
+                         2. Press 2 to singin
+                         3. Press 3 to write a post
+                         4. Press 4 to message a friend
+                         5. Press any other key to exit""")
+        
+        if user_input=="1":
+            pass
+        elif user_input=="2":
+            pass
+        elif user_input=="3":
+            pass
+        elif user_input=="4":
+            pass
+        else:
+            exit()
+
+obj = chitchat()
